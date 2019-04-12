4e8b278773ede612a95cd588a5a08b7483d3b3ae
Michael Hottman <hottmanmichael@gmail.com>
2019-04-11 19:42:25 -0600

    ensure reducer is function before calling

--- src/utils/create_reducer.js
+++ src/utils/create_reducer.js
@@ -1,8 +1,16 @@
+import { isFunction } from './type_check';
+
 export default function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.mockType)) {
+    if (
+      handlers.hasOwnProperty(action.mockType) &&
+      isFunction(handlers[action.mockType])
+    ) {
       return handlers[action.mockType](state, action);
-    } else if (handlers.hasOwnProperty(action.type)) {
+    } else if (
+      handlers.hasOwnProperty(action.type) &&
+      isFunction(handlers[action.type])
+    ) {
       return handlers[action.type](state, action);
     } else {
       return state;
