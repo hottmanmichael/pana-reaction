cf584680087ad1477f6abea8b424ff1cd06e1ebc
Michael Hottman <hottmanmichael@gmail.com>
2019-04-12 11:51:13 -0600

    refactor createFlow method for readability

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Default Library
 import Reaction from './src/Reaction';
-
 export default Reaction;
+
+// Helpers
+export ensureRequiredParams from './src/utils/ensure_required_params';
--- src/utils/create_flow.js
+++ src/utils/create_flow.js
@@ -11,50 +11,98 @@ const defaultOptions = {
   requiredParams: [],
 };
 
-export default key => {
-  let k = key.toUpperCase();
-  return function createFlow(
-    name,
-    action = noop,
-    reducer = null,
-    options = defaultOptions
+const formatActionNamesFromString = (name, reducerKey, options) => {
+  const actionName = Case.camel(name);
+  let constantKey = Case.constant(name);
+
+  if (options && options.async) {
+    constantKey = `[SAGA]/${constantKey}`;
+  }
+
+  const actionType = `${reducerKey}/${constantKey}`;
+
+  return { actionName, actionType, constantKey };
+};
+
+const formatActionNamesFromObject = (nameObject, reducerKey, options) => {
+  const actionName = nameObject.actionName;
+  const constantKey = nameObject.constant;
+  let actionType = constantKey;
+  if (nameObject.merge) {
+    actionType = `${reducerKey}/${constantKey}`;
+  }
+  return { actionName, actionType, constantKey };
+};
+
+const formatActionNames = (name, reducerKey, options = {}) => {
+  if (isString(name))
+    return formatActionNamesFromString(name, reducerKey, options);
+  else if (matchesNameObjectConvention(name))
+    return formatActionNamesFromObject(name, reducerKey, options);
+  else {
+    throw new Error(
+      `Invalid param used in ${reducerKey}.createFlow - Expected String or {actionName, constant} but received ${name} instead.`
+    );
+  }
+};
+const matchesNameObjectConvention = obj => {
+  return (
+    obj &&
+    obj.hasOwnProperty &&
+    obj.hasOwnProperty('actionName') &&
+    obj.hasOwnProperty('constant')
+  );
+};
+
+const validateAction = (action, actionName) => {
+  if (!isFunction(action) && action !== null) {
+    const message = `${k}/${actionName} has an invalid action. Action must be a function or null.`;
+    throw new Error(message);
+  }
+};
+
+const validateReducer = (reducer, actionName) => {
+  if (
+    !isFunction(reducer) &&
+    reducer !== null &&
+    'undefined' !== typeof reducer
   ) {
-    let _actionName, _constantKey;
-    let _options = Object.assign({}, defaultOptions, options);
+    const message = `${k}/${actionName} has an invalid reducer. Reducer must be a function, null, or undefined.`;
+    throw new Error(message);
+  }
+};
 
-    if (isString(name)) {
-      _actionName = Case.camel(name);
-      _constantKey = Case.constant(name);
-    } else if (name && name.actionName && name.constant) {
-      _actionName = name.actionName;
-      _constantKey = name.constant;
-    } else {
-      // FIXME: Throw warning for invalid name param
-      throw new Error(
-        `Invalid param used in ${key}.createFlow - Expected String or {actionName, constant} but received ${name} instead.`
-      );
-    }
+export default key => {
+  let reducerKey = key.toUpperCase();
 
-    if (!isFunction(action) && action !== null)
-      console.warn(
-        `${k}/${_actionName} has an invalid action. Action must be a function or null.`
-      );
+  return function createFlow(
+    createFlowName,
+    createFlowAction = noop,
+    createFlowReducer = null,
+    createFlowOptions = defaultOptions
+  ) {
+    // Merge with default options
+    const options = Object.assign({}, defaultOptions, createFlowOptions);
 
-    if (_options.async) {
-      _constantKey = `[SAGA]/${constantKey}`;
-    }
+    // Format names
+    const { actionName, constantKey, actionType } = formatActionNames(
+      createFlowName,
+      reducerKey,
+      options
+    );
 
-    const actionType =
-      isString(name) || (name && name.merge)
-        ? `${k}/${_constantKey}`
-        : _constantKey;
+    // Validate Params
+    validateAction(createFlowAction, actionName);
+    validateReducer(createFlowReducer, actionName);
 
-    const constant = actionType;
+    // Create bounded action with ensured params wrapper
     const boundAction = function bindAction() {
-      const payload = isFunction(action) ? action.apply(null, arguments) : {};
+      const payload = isFunction(createFlowAction)
+        ? createFlowAction.apply(null, arguments)
+        : {};
       return ensureRequiredParams({
-        actionName: name,
-        requiredParams: _options.requiredParams,
+        actionName: createFlowName,
+        requiredParams: options.requiredParams,
         action: () => ({
           type: actionType,
           ...payload,
@@ -65,17 +113,17 @@ export default key => {
     return {
       // Public API
       // Useful for testing or calling directly if needed
-      reducer,
-      constant,
+      actionName,
+      reducer: createFlowReducer,
+      constant: actionType,
       action: boundAction,
-      actionName: _actionName,
 
       // Internal API
       // Used to spread onto existing Reaction properties
       _internal: {
-        Action: { [_actionName]: boundAction },
-        Reducer: { [`${k}/${_constantKey}`]: reducer },
-        Constant: { [`${_constantKey}`]: `${k}/${_constantKey}` },
+        Action: { [actionName]: boundAction },
+        Reducer: { [`${reducerKey}/${constantKey}`]: createFlowReducer },
+        Constant: { [`${constantKey}`]: `${reducerKey}/${constantKey}` },
       },
     };
   };
